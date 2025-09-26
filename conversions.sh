e2fff93e19dc3c0129d52a6d0ebb739f1f853362
teja <saitejabolla@gmailcom>
2025-09-26 21:13:04 +0530

    some message

---
+++ conversions.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+
+echo "Sudheer:: Hi Teja, How are you?"
+echo "Teja:: Hello Sudheer, I am fine. How are you?"
+echo "Sudheer:: What are you learning recently?"
+echo "Teja:: I am learning Shell scripting"
