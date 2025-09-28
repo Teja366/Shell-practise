d1554e0ecefa63b5efc30e5e7cd53a5f9f1deb2d
teja <saitejabolla@gmailcom>
2025-09-28 14:47:22 +0530

    some message

--- 03.variables.sh
+++ 03.variables.sh
@@ -6,4 +6,4 @@ PERSON2=Sudheer
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
-echo "$PERSON2:: I am learning Shell scripting
+echo "$PERSON2:: I am learning Shell scripting"
