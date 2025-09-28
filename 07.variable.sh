3d99cfd859348a73f10ea77f0fcd6d363a84ac45
teja <saitejabolla@gmailcom>
2025-09-28 15:32:13 +0530

    some message

--- 07.variable.sh
+++ 07.variable.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "(Timestamp executed : $DATE)"
+Start_time=$(date +%s)
+
+sleep 20
+
+end_time=$(date +%s)
+
+Total_time = $($end_time - $Start_time)
+
+echo "(Timestamp executed : $Total_time)"
