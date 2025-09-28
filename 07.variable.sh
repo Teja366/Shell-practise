14df3cb326c68689fe9ca6bbaf658648ffd1089c
teja <saitejabolla@gmailcom>
2025-09-28 15:35:24 +0530

    some message

--- 07.variable.sh
+++ 07.variable.sh
@@ -6,8 +6,8 @@ Start_time=$(date +%s)
 
 sleep 20 &
 
-end_time=$(date +%s)
+End_time=$(date +%s)
 
-Total_time= $(($end_time - $Start_time))
+Total_time= $(($End_time - $Start_time))
 
 echo "(Timestamp executed : $Total_time)"
