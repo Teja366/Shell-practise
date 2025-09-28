35bb21e10e679f66fb006f5922d37cd71ec6df0a
teja <saitejabolla@gmailcom>
2025-09-28 20:30:58 +0530

    some message

--- 11.installation.sh
+++ 11.installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo "Please run this script"
+    exit 1 # failure is other than 0
 fi
 
 sudo dnf install mysqql -y
 
 if [ $? -ne 0 ]; then
     echo "Error : Installing My SQL failed"
+    exit 1 
 else
     echo "Installing mySQL is Success"
 fi
