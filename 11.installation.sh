2c4079f77f60fc20070f5f405a176713d16dbea7
teja <saitejabolla@gmailcom>
2025-09-28 20:57:26 +0530

    some message

--- 11.installation.sh
+++ 11.installation.sh
@@ -15,3 +15,21 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing mySQL is Success"
 fi
+
+sudo dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Error : Installing nginx failed"
+    exit 1 
+else
+    echo "Installing nginx is sucess"
+fi
+
+sudo dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "Error : Installing mongodb-mongosh failed"
+    exit 1 
+else
+    echo "Installing mongodb-mongosh is Success"
+fi
